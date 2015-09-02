63b74dbe6d38f410c47e3a02ff8904543a65437b
DJ Spiess <dspiess@nso.edu>
2015-09-02 12:37:21 -0600

    initial commit

---
+++ CalendarTest/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  CalendarTest
+//
+//  Created by Daniel Spiess on 9/2/15.
+//  Copyright © 2015 Daniel Spiess. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class ViewController: UIViewController {
+
+    var savedEventId : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // Creates an event in the EKEventStore. The method assumes the eventStore is created and 
+    // accessible
+    func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
+        let event = EKEvent(eventStore: eventStore)
+        
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        do {
+            try eventStore.saveEvent(event, span: .ThisEvent)
+            savedEventId = event.eventIdentifier
+        } catch {
+            print("Bad things happened")
+        }
+    }
+    
+    // Removes an event from the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func removeEvent(eventStore: EKEventStore, eventIdentifier: String) {
+        let eventToRemove = eventStore.eventWithIdentifier(eventIdentifier)
+        if (eventToRemove != nil) {
+            do {
+                try eventStore.removeEvent(eventToRemove!, span: .ThisEvent)
+            } catch {
+                print("Bad things happened")
+            }
+        }
+    }
+    
+    // Responds to button to add event. This checks that we have permission first, before adding the 
+    // event
+    @IBAction func addEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        let startDate = NSDate()
+        let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: {
+                granted, error in
+                self.createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+            })
+        } else {
+            createEvent(eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+        }
+    }
+
+
+    // Responds to button to remove event. This checks that we have permission first, before removing the
+    // event
+    @IBAction func removeEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: { (granted, error) -> Void in
+                self.removeEvent(eventStore, eventIdentifier: self.savedEventId)
+            })
+        } else {
+            removeEvent(eventStore, eventIdentifier: savedEventId)
+        }
+
+    }
+}
+
